bc1a13c009d5aca21c3ae5d1610f912926500609
jongoodey <goodeyjon@hotmail.com>
2013-09-20 14:03:51 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freshfun::Application.config.secret_token = '27e8e4a40581aeeb3bd246b6f7237f2bcc15c02f1fc98098f2bea9cf0c2534bbc946563f06e35d1ca20cf0b1580e3a9b8a93130cbd5300c8eeee824f2480266a'
