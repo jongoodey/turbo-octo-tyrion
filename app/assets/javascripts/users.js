2e4c035ecf800b2a3b02b54ab75d15be3821ff7b
jongoodey <goodeyjon@hotmail.com>
2013-09-22 22:22:56 +0100

    added js sdk fb

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+  FB.Event.subscribe('edge.create',
+    function(response) {
+        alert('You liked the URL: ' + response);
+    }
+  );
  
     FB.login(function(response) {
       if (response.session) {
