9db8f0af0d98bf687965be0a9bde9af999511832
jongoodey <goodeyjon@hotmail.com>
2013-09-23 00:13:09 +0100

    added callback

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,30 +1,23 @@
 $(document).ready(function(){
  
-    // FB.login(function(response) {
-    //   if (response.session) {
- 
-    //       var user_id = response.session.uid;
-    //       var page_id = "156550491219971"; //TefalUK
-    //       var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
-    //       var the_query = FB.Data.query(fql_query);
- 
-    //       the_query.wait(function(rows) {
- 
-    //           if (rows.length == 1 && rows[0].uid == user_id) {
-    //               $("#container_like").show();
- 
-    //               //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
- 
-    //           } else {
-    //               $("#container_notlike").hide();
-    //               //and here you could get the content for a non liker in ajax...
-    //           }
-    //       });
- 
- 
-    //   } else {
-    //     // user is not logged in
-    //   }
-    // });
+    window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 156550491219971, // App ID
+      channelUrl : 'http://freshfun.herokuapp.com/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+    // Additional init code here
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
  
 });
