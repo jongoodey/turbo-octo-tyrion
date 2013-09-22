dee1901a6fa0c37131df12d2adf1416e46dc6383
jongoodey <goodeyjon@hotmail.com>
2013-09-23 00:27:20 +0100

    added callback

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  
     window.fbAsyncInit = function() {
     FB.init({
-      appId      : 156550491219971, // App ID
+      appId      : '156550491219971', // App ID
       channelUrl : 'http://freshfun.herokuapp.com/channel.html', // Channel File
       status     : true, // check login status
       cookie     : true, // enable cookies to allow the server to access the session
