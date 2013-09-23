f58f658435cef8d9d50aacc8e2ffee1c0ea94cc5
jongoodey <goodeyjon@hotmail.com>
2013-09-23 11:17:25 +0100

    changes to the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
-      format.js
     end
   end
 
@@ -48,6 +47,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
+        format.js
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :message
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :message, presence: true
 end
