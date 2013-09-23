93eece5bcf5ee5d62e88da5b8d6421b40fea3371
jongoodey <goodeyjon@hotmail.com>
2013-09-23 12:46:24 +0100

    changes to the main page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-  console.log($('.fb-like').html());
+$(document).ready(function() {
+    $('#user_email').keyup(function(){
+      $('#mce-EMAIL').val($(this).val())
+    });
+    $('#user_first_name').keyup(function(){
+      $('#mce-EMAIL').val($(this).val())
+    });
+    $('#user_last_name').keyup(function(){
+      $('#mce-EMAIL').val($(this).val())
+    });
 });
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :message
   validates :email, presence: true
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :message, presence: true
